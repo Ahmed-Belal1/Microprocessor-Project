bd64ac7bb7154fcba7786c8c845a30e7bd0e333f
Ahmed-Belal1 <ahmed.belal.dawood@gmail.com>
2022-01-03 16:05:01 +0200

    Done

--- src/CommonBus.java
+++ src/CommonBus.java
@@ -1,34 +1,33 @@
 import java.util.ArrayList;
 
 public class CommonBus {
-	
+
 	ArrayList<StationItem> bus;
-	
+
 	public CommonBus() {
-		this.bus=new ArrayList<StationItem>();
+		this.bus = new ArrayList<StationItem>();
 	}
-	
-	
-	public void updateTag(String tag,float data) {
-		for (StationItem s:bus ) {
+
+	public void updateTag(String tag, float data) {
+		for (int i = 0; i < bus.size(); i++) {
+			StationItem s = bus.get(i);
 			if (s.firstWaiting.equals(tag)) {
-				s.firstWaiting="";
-				s.firstValue=data;
-			}else if (s.secondWaiting.equals(tag)) {
-				s.secondValue=data;
-				s.secondWaiting="";
+				s.firstWaiting = "";
+				s.firstValue = data;
+			} else if (s.secondWaiting.equals(tag)) {
+				s.secondValue = data;
+				s.secondWaiting = "";
 			}
-			
+
 			if (s.firstWaiting.equals("") && s.secondWaiting.equals("")) {
 				bus.remove(s);
+				i--;
 			}
-			if( bus.isEmpty()  )
-				break;
+
 		}
-		
-		
+
 	}
-	
+
 	public void requestUpdate(StationItem s) {
 		bus.add(s);
 	}
